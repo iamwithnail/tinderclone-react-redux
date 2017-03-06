a8234ac1e015268e763794f02622cdd39ea083a1
Chris Campbell <chris@ccampbell.co>
2017-03-06 21:51:54 +0000

    initial commit, mostly boilerplate.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,14 @@
+/**
+ * Created by chris on 06/03/2017.
+ */
+
+export default function() {
+
+    return [
+        { title: 'Harry Potter'},
+        { title: 'Infinite Jest'},
+        { title: 'The Sound and The Fury'},
+        { title: 'As I Lay Dying'},
+        { title: 'Knotty Problems'},
+    ]
+}
